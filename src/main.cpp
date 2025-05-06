42e36c1b35876a12e13a8cb8971ab26a709dfc60
Craftinators <craftinatorssx@gmail.com>
2025-05-06 01:41:59 -0400

    feat: add GLFW and GLAD support for OpenGL rendering

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,44 @@
 #include <spdlog/spdlog.h>
 #include <linmath.h>
 
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/gl.h>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
 int main() {
-    spdlog::info("Hello, World!");
-    constexpr vec2 v = { 1.0f, 2.0f };
-    spdlog::info(v);
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(512, 512, "Hello World", nullptr, nullptr);
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        spdlog::critical("Failed to load GLAD!");
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSwapInterval(1);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
