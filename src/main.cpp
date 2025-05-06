5f2ac9fa222b8df2de92311f36366deee5ef9d67
Craftinators <craftinatorssx@gmail.com>
2025-05-05 20:37:11 -0400

    init

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "linmath_fmt.h"
+#include "spdlog/spdlog.h"
+#include "linmath.h"
+
+int main() {
+    spdlog::info("Hello, World!");
+    constexpr vec2 v = { 1.0f, 2.0f };
+    spdlog::info(v);
+}
